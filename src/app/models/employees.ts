7082f3146dc46b8d21063118be1965df5c79c6e5
DiegoBarros12 <diegovillarroel14@gmail.com>
2024-10-15 17:06:07 -0300

    creando componenes y modulos para implementar Scss

---
+++ src/app/models/employees.ts
@@ -0,0 +1,7 @@
+export interface Employees{
+    id: string,
+    salario: number,
+    plantel: string,
+    cargo: string,
+    img:string
+}
---
+++ src/app/models/user.model.ts
@@ -0,0 +1,6 @@
+export interface User{
+    uid: string,
+    nombre: string,
+    password: string,
+    img:string
+}
