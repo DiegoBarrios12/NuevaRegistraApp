66b2181b8e596a32cc26826e998c45ac3bbd9dfb
DiegoBarros12 <diegovillarroel14@gmail.com>
2024-10-24 18:58:08 -0300

    registro validado

--- src/app/pages/auth/sing-up/sing-up.page.ts
+++ src/app/pages/auth/sing-up/sing-up.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sing-up',
@@ -9,7 +10,18 @@ export class SingUpPage implements OnInit {
 
   constructor() { }
 
+  form= new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+  })
   ngOnInit() {
+  }
+
+  submit(){
+    if(this.form.valid){
+      console.log(this.form)  
+    }
+    
   }
 
 }
